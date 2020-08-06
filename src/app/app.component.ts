a1aa2cc0c37859c4c9a4945e45ed36d5b6500e8a
Hannes Rüger <hannesrueger@gmx.de>
2020-08-06 15:47:20 +0200

    added app base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'diy-flightcase-calculator';
+    public extras = [
+        {
+            name: '8 Kugel-Ecken',
+            link: 'https://www.thomann.de/de/adam_hall_4107_kugelecke.htm',
+            price: 8 * 1.07,
+        },
+        {
+            name: '4 Rollen (2x mit Bremse)',
+            link: 'https://www.thomann.de/de/millenium_blue_wheel_bundle_80_mm.htm',
+            price: 21.40,
+        },
+        {
+            name: '4 Gummifüße',
+            link: 'https://www.thomann.de/de/adam_hall_4911_gummifuss_pack.htm',
+            price: 3.70,
+        },
+        {
+            name: '2 Tragegriffe',
+            link: 'https://www.thomann.de/de/adam_hall_34082_klappgriff.htm',
+            price: 2 * 3.61,
+        },
+        {
+            name: '2 weitere Tragegriffe',
+            link: 'https://www.thomann.de/de/adam_hall_34082_klappgriff.htm',
+            price: 2 * 3.61,
+        },
+        {
+            name: '2 Butterfly Verschlüsse',
+            link: 'https://www.thomann.de/de/adam_hall_17295_butterfly.htm',
+            price: 2 * 5.40,
+        },
+        {
+            name: '2 Scharniere',
+            link: 'https://www.thomann.de/de/adam_hall_2524_feststellscharnier.htm',
+            price: 2 * 1.46,
+        }
+    ];
+    public woodPricePerM2 = 14.06;
+    public woodLink = 'https://www.hornbach.de/shop/Mulitplexplatte-Birke-BB-BB-1500x3000x9-mm/10284886/artikel.html';
+    public profilePricePerM = 2.83;
+    public profileLink = 'https://www.thomann.de/de/adam_hall_6105.htm';
+    public dimensions = {
+        length: 100,
+        width: 50,
+        height: 50,
+    };
+
+    public getWoodArea(): number {
+        return (
+            (this.dimensions.width * this.dimensions.height * 2) +
+            (this.dimensions.width * this.dimensions.length * 2) +
+            (this.dimensions.height * this.dimensions.length * 2)
+        ) / 10000;
+    }
+
+    public getProfileLength(): number {
+        return (
+            this.dimensions.width +
+            this.dimensions.length +
+            this.dimensions.height
+        ) * 4 / 100;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
 import { AppComponent } from './app.component';
+registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'de-DE'
+  },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
