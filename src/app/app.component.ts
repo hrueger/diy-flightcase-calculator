02527780a38f3942eb4f6e29dc62a6da6d6ed6e1
Hannes Rüger <hannesrueger@gmx.de>
2020-08-06 15:58:32 +0200

    added extras to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,37 +10,51 @@ export class AppComponent {
         {
             name: '8 Kugel-Ecken',
             link: 'https://www.thomann.de/de/adam_hall_4107_kugelecke.htm',
-            price: 8 * 1.07,
+            price: 1.07,
+            count: 8,
+            checked: false,
         },
         {
             name: '4 Rollen (2x mit Bremse)',
             link: 'https://www.thomann.de/de/millenium_blue_wheel_bundle_80_mm.htm',
             price: 21.40,
+            count: 1,
+            checked: false,
         },
         {
             name: '4 Gummifüße',
             link: 'https://www.thomann.de/de/adam_hall_4911_gummifuss_pack.htm',
             price: 3.70,
+            count: 1,
+            checked: false,
         },
         {
             name: '2 Tragegriffe',
             link: 'https://www.thomann.de/de/adam_hall_34082_klappgriff.htm',
-            price: 2 * 3.61,
+            price: 3.61,
+            count: 2,
+            checked: false,
         },
         {
             name: '2 weitere Tragegriffe',
             link: 'https://www.thomann.de/de/adam_hall_34082_klappgriff.htm',
-            price: 2 * 3.61,
+            price: 3.61,
+            count: 2,
+            checked: false,
         },
         {
             name: '2 Butterfly Verschlüsse',
             link: 'https://www.thomann.de/de/adam_hall_17295_butterfly.htm',
-            price: 2 * 5.40,
+            price: 5.40,
+            count: 2,
+            checked: false,
         },
         {
             name: '2 Scharniere',
             link: 'https://www.thomann.de/de/adam_hall_2524_feststellscharnier.htm',
-            price: 2 * 1.46,
+            price: 1.46,
+            count: 2,
+            checked: false,
         }
     ];
     public woodPricePerM2 = 14.06;
